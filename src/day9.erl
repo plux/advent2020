9a19ba52885c90df5d2a0b50ed948935e2bad33d
Hakan Nilsson <haakan@gmail.com>
2020-12-09 10:12:20 +0100

    Day 9

---
+++ src/day9.erl
@@ -0,0 +1,56 @@
+-module(day9).
+-compile([export_all]).
+-include("aoc.hrl").
+
+
+solve(Input) ->
+    {part1(?ints(Input)), part2(?ints(Input))}.
+
+part1(Ints) ->
+    first_valid(25, Ints).
+
+first_valid(Size, Ints) ->
+    {Preamble, [N|_]} = lists:split(Size, Ints),
+    Sums = [X + Y || X <- Preamble, Y <- Preamble],
+    case lists:member(N, Sums) of
+        false -> N;
+        true  -> first_valid(Size, tl(Ints))
+    end.
+
+part2(Ints) ->
+    Valid = first_valid(25, Ints),
+    find_weakness(array:from_list(Ints), Valid, 0, 1).
+
+find_weakness(Nums, Num, Start, Len) ->
+    Set = [array:get(I, Nums) || I <- lists:seq(Start, Start+Len)],
+    Sum = lists:sum(Set),
+    if Sum == Num -> lists:min(Set) + lists:max(Set);
+       Sum >  Num -> find_weakness(Nums, Num, Start+1, 1);
+       Sum <  Num -> find_weakness(Nums, Num, Start, Len+1)
+    end.
+
+-include_lib("eunit/include/eunit.hrl").
+solve_test_() ->
+    L = [35,
+         20,
+         15,
+         25,
+         47,
+         40,
+         62,
+         55,
+         65,
+         95,
+         102,
+         117,
+         150,
+         182,
+         127,
+         219,
+         299,
+         277,
+         309,
+         576],
+    [ ?_assertEqual(127, first_valid(5, L))
+    , ?_assertEqual({18272118, 2186361}, ?solve())
+    ].
