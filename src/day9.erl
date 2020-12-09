80c6402914e2b0179a65326f1e97e31d6f1dd12d
Hakan Nilsson <haakan@gmail.com>
2020-12-09 16:00:38 +0100

    Optimize Day 9

--- src/day9.erl
+++ src/day9.erl
@@ -19,14 +19,15 @@ first_valid(Size, Ints) ->
 
 part2(Ints) ->
     Valid = first_valid(25, Ints),
-    find_weakness(array:from_list(Ints), Valid, 0, 1).
+    find_weakness(array:from_list(Ints), Valid, 0, 0, [], 0).
 
-find_weakness(Nums, Num, Start, Len) ->
-    Set = [array:get(I, Nums) || I <- lists:seq(Start, Start+Len)],
-    Sum = lists:sum(Set),
+find_weakness(Nums, Num, Start, I, Set0, Sum0) ->
+    Elem = array:get(I, Nums),
+    Sum = Elem + Sum0,
+    Set = [Elem|Set0],
     if Sum == Num -> lists:min(Set) + lists:max(Set);
-       Sum >  Num -> find_weakness(Nums, Num, Start+1, 1);
-       Sum <  Num -> find_weakness(Nums, Num, Start, Len+1)
+       Sum >  Num -> find_weakness(Nums, Num, Start+1, Start+1, [], 0);
+       Sum <  Num -> find_weakness(Nums, Num, Start, I+1, Set, Sum)
     end.
 
 -include_lib("eunit/include/eunit.hrl").
